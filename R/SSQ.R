847f8bd44b38b67e495c3bf1c2f0f936c7c61d53
parksw3 <parksw3@mcmaster.ca>
2016-05-28 21:41:17 +0900

    tweaks

--- R/SSQ.R
+++ R/SSQ.R
@@ -7,17 +7,15 @@ eqV = sL$eq_vir
 peakV = sL$peak_vir
 
 lmer(eqV ~ 1 + (1|model), data = sL, REML=FALSE)
-lmer(peakV ~ 1 + (1|model), data = sL, REML=FALSE)
+lmer(peakV ~ 1 + (1|model), data = sL, REML=FALSE) 
 
 findSSQ <- function(var){
 	wSSQ = sum((var-ave(var, model))^2) ##within
 	group_mean = tapply(var,list(model),mean)
 	bSSQ = sum(((group_mean)-mean(var))^2)*1000 ##between
-	total = wSSQ + bSSQ
-	wR = wSSQ/total
-	bR = bSSQ/total
-	cat("within:", wR,"\n")
-	cat("between:", bR,"\n")
+	
+	cat("within model standard deviation:", sqrt(wSSQ/6000),"\n")
+	cat("between model standard deviation:", sqrt(bSSQ/6000),"\n")
 }
 
 findSSQ(eqV)
