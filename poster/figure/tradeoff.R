9955c50126889ac5ba2de4bc49897194c7a31d5b
Ben Bolker <bbolker@gmail.com>
2016-05-30 14:59:07 -0400

    restructure poster; drop URLs from bib

---
+++ poster/figure/tradeoff.R
@@ -0,0 +1,13 @@
+source("fig_setup.R")
+
+dd <-with(pars,
+     melt(data.frame(alpha=alpha,"transmission probability"=Beta2,
+                     "duration (years)"=Duration2,check.names=FALSE),
+          id.vars="alpha"))
+gg_tradeoff <- ggplot(dd,aes(alpha,value))+facet_wrap(~variable,scale="free")+
+    geom_line(size=10)+
+    labs(x= Lspvl,y="")
+
+tikz(file="tradeoff.tikz",width=12,height=8,standAlone=TRUE)
+print(gg_tradeoff)
+dev.off()
---
+++ poster/figure/univ.R
@@ -0,0 +1,28 @@
+source("fig_setup.R")
+
+sL <- transform(sum_list[["sum_mat"]],rel_peak=peak_vir/eq_vir)
+sL$model <- factor(sL$model, c("random","pairform+epc", "pairform", "instswitch+epc","instswitch" , "implicit"))
+mL <- melt(sL,id.vars=c("model","run"))
+## horrible hack (but doesn't help); subsample
+## w <- which(mL$model=="random")
+## mLw <- mL[-sample(w,size=length(w)*9/10,replace=FALSE),]
+w <- with(mL,which(model=="random" & variable=="eq_vir"))
+rval <- mean(mL$value[w])
+mLw <- droplevels(mL[-w,])
+mLw$variable <- fixfac2(mLw$variable)
+
+gg_univ <- ggplot(mLw,aes(value,model,fill=model))+
+    geom_violinh(width=1)+
+    facet_wrap(~variable,scale="free_x")+
+    guides(fill=guide_legend(reverse=TRUE))+
+    labs(y="",x="")+
+    geom_point(data=data.frame(model="random",
+                               variable="equilibrium virulence",
+                               value=rval),
+               pch=22,size=8,show.legend=FALSE) +
+    theme(legend.position="none")+
+    zero_x_margin
+
+tikz(file="univ.tikz",width=12,height=10,standAlone=TRUE)
+print(gg_univ)
+dev.off()
