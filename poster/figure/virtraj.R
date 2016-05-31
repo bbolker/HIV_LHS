c13dfd841bba644780ecdcaef17f7cc781ffd70d
Ben Bolker <bbolker@gmail.com>
2016-05-31 11:05:24 -0400

    further poster fussing

--- poster/figure/virtraj.R
+++ poster/figure/virtraj.R
@@ -8,7 +8,7 @@ gg_virtraj <- ggplot(ss,
                      aes(tvec,mean,ymin=lwr,ymax=upr))+
     geom_line(aes(colour=model,linetype=model),lwd=1)+
     geom_ribbon(aes(fill = model), alpha=0.3)+
-    labs(x="time (years)",y=Lspvl) +
+    labs(x="time (years)",y="population mean set-point viral load ($\\log_{10}$)") +
     theme(axis.title.y = element_text(margin = margin(0,10,0,0)),
           axis.title.x = element_text(margin = margin(10,0,0,0))) +
     facet_wrap(~model, nrow = 3) +
