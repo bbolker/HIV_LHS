471d581f482bc8767bd4855350569a6408bd6d3d
Ben Bolker <bbolker@gmail.com>
2016-06-10 09:08:45 -0400

    to do

--- R/SSQ.R
+++ R/SSQ.R
@@ -7,7 +7,7 @@ eqV = sL$eq_vir
 peakV = sL$peak_vir
 
 m_peakV <- lmer(peakV ~ 1 + (1|model), data = sL, REML=FALSE)
-v_among <- VarCorr(m_peakV)[[1]]
+v_among <- c(VarCorr(m_peakV)[[1]])  ## c() dumps extra information
 v_within <- sigma(m_peakV)^2
 v_among/(v_among+v_within)
 
@@ -28,3 +28,13 @@ findSSQ(eqV)
 findSSQ(peakV)
 
 ##why is this different from the lmer results?
+
+## do some calculations on the scales of transmission prob/duration as
+## well as on the log10 SPVL scale, for example.
+##
+##    calculate the peak transmission probability and the 95% quantiles
+##    of peak transmission probability for the pairform+epc model
+##
+##    ditto for the implicit model
+
+##    ditto for duration
