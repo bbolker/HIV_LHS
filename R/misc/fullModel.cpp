af899a783c41c740dba18708004ceac88c788c2a
parksw3 <parksw3@mcmaster.ca>
2016-09-12 12:16:04 -0400

    fixed fullModel.cpp

--- R/misc/fullModel.cpp
+++ R/misc/fullModel.cpp
@@ -11,90 +11,103 @@ CharacterVector getListElement(List stuff) {
 
 // getListElement(list(a="abc"))
 
+//[[Rcpp::export]]
 List g(double t, NumericVector yini, List parameters,
-		NumericMatrix cc_mat) {
+       NumericMatrix cc_mat) {
+	
+	int i,j,k; // counters
 
-    int i,j,k; // counters
+  // http://gallery.rcpp.org/articles/subsetting/    
 
-    // http://gallery.rcpp.org/articles/subsetting/    
+  // should we (1) pass 1 long vector and split it?
+  //           (2) pass 1 long vector and use vector indexing (maybe 
+  // .. #define n(i) N[i]
+  // .. #define nn(j,k) N[nrisk+k*nrisk+j]
 
-    // should we (1) pass 1 long vector and split it?
-    //           (2) pass 1 long vector and use vector indexing (maybe 
-    // .. #define n(i) N[i]
-    // .. #define nn(j,k) N[nrisk+k*nrisk+j]
+  // FIXME: is there a more compact but still reasonably clear
+  //    way to do this?  macro def?
+  int nrisk = as<int>(parameters["n.risk"]);
+  NumericVector rrisk(nrisk);
+  rrisk = parameters["r.risk"];
+  
+  double rho = as<double>(parameters["rho"]);
+	
+  // N <- yini[1:n.risk]
+  NumericVector N(nrisk);
+  
+  for (i=0; i<nrisk; i++){
+  	
+  	N[i] = yini[i];
+  }
+  
 
-    // FIXME: is there a more compact but still reasonably clear
-    //    way to do this?  macro def?
-    int nrisk = as<int>(parameters["n.risk"]);
-    double rrisk = as<double>(parameters["r.risk"]);
-    double rho = as<double>(parameters["rho"]);
+  // copy last (nrisk^2) elements into NN matrix
+  // NN <- matrix(yini[-(1:n.risk)], n.risk, n.risk)
+  // NN_adj <- NN
+  // should be able to replace this with a single incantation
+  // something like std::fill(NN.begin(), NN.end(), N[nrisk,N.end()]);
 
-    // N <- yini[1:n.risk]
-    NumericVector N(nrisk);
-    for (i=0; i<nrisk; i++) 
-	N(i) = yini(i);
+  NumericMatrix NN(nrisk,nrisk);
+  NumericMatrix NN_adj(nrisk,nrisk);
+	
+ 	for (i=0; i<nrisk; i++){
+ 		k = i * nrisk;
+ 		
+ 		for (j=0; j<nrisk; j++)  {
+ 			NN_adj = NN(i,j) = N[nrisk+k];
+ 			k++;
+ 		}
+ 	}
+		
+  // double diagonal
+  // diag(NN_adj) <- 2 * diag(NN_adj)
+  for (i=0; i<nrisk; i++){
+  	NN_adj(i,i) *= 2;
+  }
+	
+  //           frate <- r.risk * rho * N
+  //           f.sum <- sum(frate)
+	
+  NumericVector frate(nrisk);
+ 	frate = rrisk*rho*N;
+  double fsum = sum(frate);
 
-    // copy last (nrisk^2) elements into NN matrix
-    // NN <- matrix(yini[-(1:n.risk)], n.risk, n.risk)
-    // NN_adj <- NN
-    // should be able to replace this with a single incantation
-    // something like std::fill(NN.begin(), NN.end(), N[nrisk,N.end()]);
+  NumericVector dN(nrisk);
 
-    NumericMatrix NN(nrisk,nrisk);
-    NumericMatrix NN_adj(nrisk,nrisk);
-
-    for (i=0; i<nrisk; i++) 
-	for (j=0; j<nrisk; j++)  {
-	    NN_adj = NN(i,j) = N[nrisk+k];
-	    k++;
-	}
-    // double diagonal
-    // diag(NN_adj) <- 2 * diag(NN_adj)
-    for (i=0; i<nrisk; i++) 
-	NN_adj(i,i) *= 2;
-
-    //           frate <- r.risk * rho * N
-    //           f.sum <- sum(frate)
-
-    NumericVector frate(nrisk);
-    frate = rrisk*rho*N;
-    double fsum = sum(frate);
-
-    NumericVector dN(nrisk);
-
-    //           dN <- - frate + colSums(cc_mat * NN_adj)
-    for (i=0; i<nrisk; i++) {
-	dN(i) = -frate(i);
-    	for (int j=0; j<nrisk; j++) {
+  //           dN <- - frate + colSums(cc_mat * NN_adj)
+  for (i=0; i<nrisk; i++) {
+		dN(i) = -frate(i);
+    for (int j=0; j<nrisk; j++) {
 	    dN[i] += cc_mat(i,j)*NN_adj(i,j);
-	}
-    }
+		}
+  }
 
-    //           f.NN <- outer(frate, frate, "*")/f.sum 
-    //           diag(f.NN) <- diag(f.NN)/2
-    NumericMatrix fNN(nrisk,nrisk);
-    for (i=0; i<nrisk; i++)  {
-	for (j=0; j<nrisk; j++) {
+  //           f.NN <- outer(frate, frate, "*")/f.sum 
+  //           diag(f.NN) <- diag(f.NN)/2
+  NumericMatrix fNN(nrisk,nrisk);
+  for (i=0; i<nrisk; i++)  {
+		for (j=0; j<nrisk; j++) {
 	    fNN(i,j) = frate(i)*frate(j)/fsum;
+			
 	    if (i==j) {
-		fNN(i,j) /= 2.0;
-	    }
-	}
-    }
+				fNN(i,j) /= 2.0;
+	  	}
+		}
+  }
     
-    // don't really need this, should just modify fNN in place ... 
-    //           dNN <- f.NN - cc_mat * NN
-    // might be vectorizable ... ?
-    NumericMatrix dNN(nrisk,nrisk);
-    for (i=0; i<nrisk; i++)  {
-	for (j=0; j<nrisk; j++) {
+  // don't really need this, should just modify fNN in place ... 
+  //           dNN <- f.NN - cc_mat * NN
+  // might be vectorizable ... ?
+  NumericMatrix dNN(nrisk,nrisk);
+  for (i=0; i<nrisk; i++)  {
+		for (j=0; j<nrisk; j++) {
 	    dNN(i,j) = fNN(i,j) - cc_mat(i,j)*NN(i,j);
-	}
-    }
-
-    //           list(c(dN, dNN))
-    List ret;
-    ret[1] = dN;
-    ret[2] = dNN;
-    return(ret);
+		}
+  }
+	
+  //           list(c(dN, dNN))
+  
+  return Rcpp::List::create(Rcpp::Named("dN") = dN,
+                           Rcpp::Named("dNN") = dNN);
+  
 }
