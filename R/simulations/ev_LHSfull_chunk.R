c68771287312e0c01b901a996cb6672baec0a9b4
Ben Bolker <bbolker@gmail.com>
2016-09-28 11:48:15 -0400

    tweak LHSfull chunk (i/mrow)

--- R/simulations/ev_LHSfull_chunk.R
+++ R/simulations/ev_LHSfull_chunk.R
@@ -32,7 +32,7 @@ eq_vecFull <- rep(0, n.trial)
 peak_matFull <- matrix(NA, nrow = n.trial, ncol = 2)
 val_vecFull <- rep(0, n.trial)
 
-
+mrow <- 1
 for (i in start_sim:end_sim) {
     cat(i)
     r <- try(
@@ -46,24 +46,26 @@ for (i in start_sim:end_sim) {
             } else {
                 interval = c(0.01,1)
             }
-            val_vecFull[i] = find_scale(0.04, full_model_rcpp, yini,
+            val_vecFull[mrow] = find_scale(0.04, full_model_rcpp, yini,
                                         HIVpars, interval=interval,
-                                        adjpar = "scale_all", full = TRUE, hmax = 0.5)
-            HIVpars_adj <- transform(HIVpars, scale_all = val_vecFull[i])
+                                        adjpar = "scale_all", full = TRUE,
+                                        hmax = 0.5)
+            HIVpars_adj <- transform(HIVpars, scale_all = val_vecFull[mrow])
             pp_adj <- expand(HIVpars_adj)
             rk(unlist(yini), func = full_model_rcpp, parms = pp_adj,
                times = tvec)
             })
     if (!inherits(r,"try-error")) {
-        I_matFull[,i] = r[,(ncol(r) - 1)]
-        vir_matFull[,i] = r[,ncol(r)]
-        eq_vecFull[i] = vir_matFull[length(tvec),i]
+        I_matFull[,mrow] = r[,(ncol(r) - 1)]
+        vir_matFull[,mrow] = r[,ncol(r)]
+        eq_vecFull[mrow] = vir_matFull[length(tvec),i]
     
-        peak_matFull[i,1] = which.max(vir_matFull[,i])
-        peak_matFull[i,2] = max(vir_matFull[,i])
+        peak_matFull[mrow,1] = which.max(vir_matFull[,mrow])
+        peak_matFull[mrow,2] = max(vir_matFull[,mrow])
 
         save("I_matFull", "vir_matFull", "eq_vecFull","peak_matFull", "val_vecFull",file=fn)
     }
+    mrow <- mrow+1
 }
 save("I_matFull", "vir_matFull", "eq_vecFull","peak_matFull", "val_vecFull",file=fn)
 
