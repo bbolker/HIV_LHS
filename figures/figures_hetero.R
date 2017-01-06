6aaa80e69f56044b0ff14b772bfbe5a3f3df056c
parksw3 <parksw3@mcmaster.ca>
2017-01-06 15:17:57 -0500

    added natural bd

--- figures/figures_hetero.R
+++ figures/figures_hetero.R
@@ -2,19 +2,20 @@ library(ggplot2); theme_set(theme_bw())
 library(dplyr)
 library(tidyr)
 load("../simdata/ev_hetero.rda")
+load("../simdata/ev_bd.rda")
 
 zero_x_margin <-
 	theme(panel.spacing.x=grid::unit(0, "lines"))
 
 tvec <- c(1:1000)
-het <- seq(1, 7, 2)
+div <- seq(1, 7, 2)
 model <- c("full+epc", "full", "instswitch+epc", "instswitch")
 
-sort_matrix <- function(mat){
-	list(hetero = data.frame(tvec, mat[,het]), 
-			 homo = data.frame(tvec, mat[,-het])) %>%
+sort_matrix <- function(mat, name){
+	list(hetero = data.frame(tvec, mat[,div]), 
+			 homo = data.frame(tvec, mat[,-div])) %>%
 		lapply(function(x) setNames(x, c("tvec", model))) %>%
-		setNames(c("hetero", "homo")) %>%
+		setNames(name) %>%
 		bind_rows(.id = "type") %>%
 		as_data_frame %>%
 		gather(key, value, -type, -tvec) %>%
@@ -29,6 +30,14 @@ g_base <- ggplot(data = NULL, aes(tvec, value, col = type)) +
 	zero_x_margin +
 	theme(panel.grid = element_blank())
 
-g_base %+% (vir_matFull %>% sort_matrix)
+## homo vs hetero
 
-g_base %+% (I_matFull %>% sort_matrix)
+g_base %+% (vir_matFull %>% sort_matrix(c("hetero", "homo")))
+
+g_base %+% (I_matFull %>% sort_matrix(c("hetero", "homo")))
+
+## natural birth and death
+
+g_base %+% (vir_mat %>% sort_matrix(c("no_bd", "bd")))
+
+g_base %+% (I_mat %>% sort_matrix(c("no_bd", "bd")))
